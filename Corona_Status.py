ccb35fd6be4c35168953cd13d10ef9a0be6cd3ac
RyanzSantos <69498364+RyanzSantos@users.noreply.github.com>
2021-12-23 21:11:26 +0700

    Update Corona_Status.py

--- Corona_Status.py
+++ Corona_Status.py
@@ -1,5 +1,7 @@
 import time, requests
 
+tok3n = "" # Put Yout Token Here
+
 while True:
     corona_api = "https://coronavirus-19-api.herokuapp.com/countries/world"
     corona_status = requests.get(corona_api)
@@ -10,5 +12,5 @@ while True:
       "custom_status": {"text": f"Covid Today Cases: {TodayCases}, Today death: {TodayDeaths}"}
     }
 
-    x = requests.patch("https://ptb.discordapp.com/api/v9/users/@me/settings", headers={"authorization": "YOUR_TOKEN_HERE"}, json=content)
+    x = requests.patch("https://ptb.discordapp.com/api/v9/users/@me/settings", headers={"authorization": tok3n}, json=content)
     time.sleep(5)
